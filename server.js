8e0c3d6285f2795c70edba0a8f94c7473209151f
OMGDuke <lex1989@gmail.com>
2016-07-14 15:07:52 +0100

    Setting up development env config

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-var express = require('express');
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var express = require('./config/express');
+
 var app = express();
-
-app.use('/', function(request, response) {
-  response.send('Hello World');
-});
-
 app.listen(3000);
-console.log('Express server running on localhost:3000');
-
 module.exports = app;
+
+console.log('Express server running on localhost:3000');
